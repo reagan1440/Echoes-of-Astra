ec79aae8c0fbf64442b2e06dc51f5d1561b2d480
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2024-03-14 20:26:50 -0400

    boilerplate minus API key?

--- Unsolved/server/schemas/resolvers.js
+++ Unsolved/server/schemas/resolvers.js
@@ -1,6 +1,5 @@
 const { User, Product, Category, Order } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
