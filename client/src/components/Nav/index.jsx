a6a8361b0ed1d238d0ff538970de53bf5481f090
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-18 20:44:13 -0400

    title

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -42,8 +42,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <span role="img" aria-label="shopping bag">🧞‍♂️</span>
+          -Echoes-Of-Astra
         </Link>
       </h1>
 
