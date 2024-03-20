71c8d36c9dc042c5d9b4fb033290c35b6cdedc81
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-20 03:41:14 -0400

    rough sketch up of the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,24 @@
 import astra from "../assets/images/Cosmog.png"
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, Center, Grid, GridItem } from "@chakra-ui/react"
 
 
 const Home = () => {
   return (
     <div className="container">
 
-{/* // adding linear gradient and color transitions
-<Box w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)" />
 
-// adding radial gradient and color transitions
-<Box w="100%" h="200px" bgGradient="radial(gray.300, yellow.400, pink.200)" />
+<Grid templateColumns='repeat(3, 1fr)' gap={100}>
+  <h1> <GridItem />Dream Journal</h1>
+  <h1> <GridItem />Dream Interpretation</h1>
+  <h1> <GridItem />Dream Dictionary</h1>
+</Grid>
 
-// adding the text gradient
-<Text
-  bgGradient="linear(to-l, #7928CA, #FF0080)"
-  bgClip="text"
-  fontSize="6xl"
-  fontWeight="extrabold"
->
-  Welcome to Chakra UI
-</Text> */}
+<Center color='white'>
+<Box boxSize='lg'>
+  <img src={astra} alt='Dream Genie' />
+</Box>
+</Center>
 
-
-// verbose
-<Box
-  backgroundImage="url('../assets/images/Cosmog.png')"
-  backgroundPosition="center"
-  backgroundRepeat="no-repeat"
-/>
-
-// shorthand
-<Box
-  bgImage="url('../assets/images/Cosmog.png')"
-  bgPosition="center"
-  bgRepeat="no-repeat"
-/>
     </div>
   );
 };
