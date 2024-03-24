98bd34c675bda864b36dc782598f55a7594092d0
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2024-03-24 15:32:54 -0400

    commented out images while working on importing them

--- client/src/pages/Dream Dictionary/DreamDictionary.jsx
+++ client/src/pages/Dream Dictionary/DreamDictionary.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import styles from "./assets/dreamdictionary.module.css";
-import chased from "../../assets/images/being-chased.png";
-import death from "../../assets/images/death.png";
-import falling from "../../assets/images/falling.png";
-import flying from "../../assets/images/flying.png";
-import late from "../../assets/images/late.png";
-import lost from "../../assets/images/lost.png";
-import naked from "../../assets/images/naked.png";
-import teeth from "../../assets/images/teeth.png";
-import test from "../../assets/images/test-taking.png";
-import water from "../../assets/images/water.png";
+// import chased from "../../assets/images/being-chased.png";
+// import death from "../../assets/images/death.png";
+// import falling from "../../assets/images/falling.png";
+// import flying from "../../assets/images/flying.png";
+// import late from "../../assets/images/late.png";
+// import lost from "../../assets/images/lost.png";
+// import naked from "../../assets/images/naked.png";
+// import teeth from "../../assets/images/teeth.png";
+// import test from "../../assets/images/test-taking.png";
+// import water from "../../assets/images/water.png";
 
 const DreamDictionary = () => {
   return (
