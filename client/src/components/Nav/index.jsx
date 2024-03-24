cc55f3405df9ffde2d7a3b569dc5015e9d627e95
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-03-24 02:42:10 -0400

    changed OrderHistory to DonationHistory and updated accordingly. Added thought bubble images, and imported to homepage, and added styling

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -9,7 +9,7 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
+            <Link to="/donationHistory">
               Make a Donation
             </Link>
           </li>
